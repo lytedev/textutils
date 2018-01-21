9ef0d519eb85c450a5f6f74662276696de4a32b9
Daniel Flanagan <daniel@lytedev.io>
2018-01-21 10:55:28 -0600

    Update dependencies and fix pre-title

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -28,6 +28,7 @@ module.exports = merge(baseWebpackConfig, {
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'index.pug',
+      title: 'TextUtils.org',
       inject: true
     })
   ]
